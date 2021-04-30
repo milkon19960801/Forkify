844bcfb066d21afe47a630d61345205f6e3e981b
milkon <melkon_2010@hotmail.com>
2021-04-30 16:38:16 +0200

    updateing the dom when increase/decrease serving instead of rendering the dom

--- src/js/controller.js
+++ src/js/controller.js
@@ -52,7 +52,8 @@ const controlPagination = function (gotoPage) {
 const controlServings = function (newServings) {
   model.updateServings(newServings);
 
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
