753c5ae61a4ea3c29bf78034340dc4f284a3286a
milkon <melkon_2010@hotmail.com>
2021-04-30 13:20:16 +0200

    added next/previous, rendering btns after pagenumber

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -34,7 +35,9 @@ const controlSearchResults = async function () {
 
     await model.loadSearchResults(query);
 
-    resultsView.render(model.getSearchResultsPage());
+    resultsView.render(model.getSearchResultsPage(1));
+
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -2,7 +2,53 @@ import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 
 class PaginationView extends View {
-  _parentElement = document.querySelector('.results');
+  _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {
+    const currentPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    console.log(numPages);
+
+    if (currentPage === 1 && numPages > 1)
+      return `
+        <button class="btn--inline pagination__btn--next">
+            <span>Page${currentPage + 1} </span>
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+        </button> 
+        `;
+
+    if (currentPage === numPages && numPages > 1)
+      return `    
+        <button class="btn--inline pagination__btn--prev">
+            <span>Page ${currentPage - 1}</span>
+            <svg class="search__icon">
+                <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+        </button>
+        `;
+
+    if (currentPage < numPages) {
+      return `
+            <button class="btn--inline pagination__btn--next">
+                <span>Page${currentPage + 1} </span>
+                <svg class="search__icon">
+                    <use href="${icons}#icon-arrow-right"></use>
+                </svg>
+            </button> 
+            <button class="btn--inline pagination__btn--prev">
+                <span>Page ${currentPage - 1}</span>
+                <svg class="search__icon">
+                    <use href="${icons}#icon-arrow-left"></use>
+                </svg>
+            </button>
+        `;
+    }
+    return ``;
+  }
 }
 
 export default new PaginationView();
