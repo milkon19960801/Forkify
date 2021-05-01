c90633ba302b1b65c39d628af833a99695098438
milkon <melkon_2010@hotmail.com>
2021-05-01 15:00:16 +0200

    showing input window when click on add recipe btn

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,30 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class addRecipeView extends View {
+  _parentElement = document.querySelector('.upload');
+
+  _window = document.querySelector('.add-recipe-window');
+  _overlay = document.querySelector('.overlay');
+
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+  }
+
+  _toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this._toggleWindow.bind(this));
+  }
+
+  _generateMarkup() {}
+}
+
+export default new addRecipeView();
